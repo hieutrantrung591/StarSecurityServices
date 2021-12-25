c1e7e050e1146e3751c42d26c7525c23e82d893a
Trần Trung Hiếu <trunghieu05092001@gmail.com>
2021-12-25 21:37:14 +0700

    Fix startup file

--- StarSecurityServices/StarSecurityServices/Startup.cs
+++ StarSecurityServices/StarSecurityServices/Startup.cs
@@ -62,6 +62,10 @@ namespace StarSecurityServices
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                  );
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
