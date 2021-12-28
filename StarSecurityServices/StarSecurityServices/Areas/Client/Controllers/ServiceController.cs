c818b9df76b61205f278d8e108f513cea2c7f3e3
loctokhuong <loctokhuong@gmail.com>
2021-12-28 21:27:06 +0700

    Add service UI

---
+++ StarSecurityServices/StarSecurityServices/Areas/Client/Controllers/ServiceController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StarSecurityServices.Areas.Client.Controllers
+{
+    [Area("Client")]
+    public class ServiceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ServiceDetails()
+        {
+            return View();
+        }
+    }
+}
