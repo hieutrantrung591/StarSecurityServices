50eb77213bf585709bdc74e6b1639b42382f2f0c
Duong Hao <duongvanhao711@outlook.com>
2022-01-01 03:00:09 +0700

    home page contact us logout

---
+++ StarSecurityServices/StarSecurityServices/Areas/Client/Controllers/LoginController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StarSecurityServices.Areas.Client.Controllers
+{
+    [Area("Client")]
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
