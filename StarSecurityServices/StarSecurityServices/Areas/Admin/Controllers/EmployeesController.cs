3df04426bab92385a127b9517803af36623ed579
Trần Trung Hiếu <trunghieu05092001@gmail.com>
2022-01-03 22:14:59 +0700

    Add Identity

---
+++ StarSecurityServices/StarSecurityServices/Areas/Admin/Controllers/EmployeesController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using StarSecurityServices.Models;
+
+namespace StarSecurityServices.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class EmployeesController : Controller
+    {
+        private readonly StarSecurityDBContext _context;
+
+        public EmployeesController(StarSecurityDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Employees
+        public async Task<IActionResult> Index()
+        {
+            var starSecurityDBContext = _context.Employees.Include(e => e.Department).Include(e => e.Job);
+            return View(await starSecurityDBContext.ToListAsync());
+        }
+
+        // GET: Admin/Employees/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employees
+                .Include(e => e.Department)
+                .Include(e => e.Job)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        // GET: Admin/Employees/Create
+        public IActionResult Create()
+        {
+            ViewData["DepartmentId"] = new SelectList(_context.Departments, "Id", "Name");
+            ViewData["JobId"] = new SelectList(_context.Jobs, "Id", "Description");
+            return View();
+        }
+
+        // POST: Admin/Employees/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Password,Address,Contact,Qualification,Grade,Achievement,JobId,DepartmentId,CreatedAt,UpdatedAt")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(employee);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DepartmentId"] = new SelectList(_context.Departments, "Id", "Name", employee.DepartmentId);
+            ViewData["JobId"] = new SelectList(_context.Jobs, "Id", "Description", employee.JobId);
+            return View(employee);
+        }
+
+        // GET: Admin/Employees/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            ViewData["DepartmentId"] = new SelectList(_context.Departments, "Id", "Name", employee.DepartmentId);
+            ViewData["JobId"] = new SelectList(_context.Jobs, "Id", "Description", employee.JobId);
+            return View(employee);
+        }
+
+        // POST: Admin/Employees/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Password,Address,Contact,Qualification,Grade,Achievement,JobId,DepartmentId,CreatedAt,UpdatedAt")] Employee employee)
+        {
+            if (id != employee.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(employee);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EmployeeExists(employee.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DepartmentId"] = new SelectList(_context.Departments, "Id", "Name", employee.DepartmentId);
+            ViewData["JobId"] = new SelectList(_context.Jobs, "Id", "Description", employee.JobId);
+            return View(employee);
+        }
+
+        // GET: Admin/Employees/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employees
+                .Include(e => e.Department)
+                .Include(e => e.Job)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        // POST: Admin/Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+            _context.Employees.Remove(employee);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EmployeeExists(int id)
+        {
+            return _context.Employees.Any(e => e.Id == id);
+        }
+    }
+}
