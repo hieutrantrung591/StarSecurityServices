6bd334f07c433dc92b2ad3cfa2a3b9f1f8e2ba0a
Trần Trung Hiếu <trunghieu05092001@gmail.com>
2022-01-05 12:12:39 +0700

    Update Client UI

--- StarSecurityServices/StarSecurityServices/wwwroot/js/site.js
+++ StarSecurityServices/StarSecurityServices/wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+/**
+      * Back to top button
+      */
+let backtotop = select('.back-to-top')
+if (backtotop) {
+    const toggleBacktotop = () => {
+        if (window.scrollY > 100) {
+            backtotop.classList.add('active')
+        } else {
+            backtotop.classList.remove('active')
+        }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+}
