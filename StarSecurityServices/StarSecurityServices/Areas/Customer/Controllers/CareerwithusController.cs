24739fae2f4955ed6fb5774ea7fdd9ebee35b554
Duong Hao <duongvanhao711@outlook.com>
2022-01-13 12:48:36 +0700

    add career with us

---
+++ StarSecurityServices/StarSecurityServices/Areas/Customer/Controllers/CareerwithusController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StarSecurityServices.Areas.Customer.Controllers
+{
+    public class CareerwithusController : Controller
+    {
+        [Area("Customer")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
