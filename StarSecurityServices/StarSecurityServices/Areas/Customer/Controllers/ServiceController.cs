07b79e575cdfd7b9a3328a8548110d20d364f09e
Trần Trung Hiếu <trunghieu05092001@gmail.com>
2022-01-14 22:12:14 +0700

    Update Career, Business page in Customer

--- StarSecurityServices/StarSecurityServices/Areas/Customer/Controllers/ServiceController.cs
+++ StarSecurityServices/StarSecurityServices/Areas/Customer/Controllers/ServiceController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StarSecurityServices.Areas.Customer.ViewModels;
+using StarSecurityServices.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,14 +12,22 @@ namespace StarSecurityServices.Areas.Customer.Controllers
     [Area("Customer")]
     public class ServiceController : Controller
     {
+        private readonly StarSecurityDBContext _context;
+
+        public ServiceController(StarSecurityDBContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
-        }
+            var serviceList = _context.Services.Include(b => b.Division);
+            var divisionList = _context.Divisions;
 
-        public IActionResult ServiceDetails()
-        {
-            return View();
+            ServiceDivisionViewModel serviceDivisionViewModel = new ServiceDivisionViewModel();
+            serviceDivisionViewModel.Services = serviceList;
+            serviceDivisionViewModel.Divisions = divisionList;
+
+            return View(serviceDivisionViewModel);
         }
     }
 }
