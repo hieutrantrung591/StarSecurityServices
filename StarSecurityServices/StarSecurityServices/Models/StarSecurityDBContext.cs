70f8c3652c1fbc18ebb49f72d2da6889f38278f1
Trần Trung Hiếu <trunghieu05092001@gmail.com>
2022-02-19 08:43:23 +0700

    Update DBContext

--- StarSecurityServices/StarSecurityServices/Models/StarSecurityDBContext.cs
+++ StarSecurityServices/StarSecurityServices/Models/StarSecurityDBContext.cs
@@ -31,14 +31,14 @@ namespace StarSecurityServices.Models
         public virtual DbSet<Service> Services { get; set; }
         public virtual DbSet<Vacancy> Vacancies { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=DESKTOP-8VDLDPR\\SQLEXPRESS;Database=StarSecurityDB;Trusted_Connection=True;");
-            }
-        }
+//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//        {
+//            if (!optionsBuilder.IsConfigured)
+//            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//                optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=StarSecurityDB;Trusted_Connection=True;");
+//            }
+//        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
